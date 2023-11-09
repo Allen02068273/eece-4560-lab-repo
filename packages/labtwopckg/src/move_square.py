8be60f03c9c0ab93170bc2e485ea9aa2a983edbf
Nolan Allen <nolan_allen@student.uml.edu>
2023-11-09 23:01:12 +0000

    move_square listens to fsm_node

--- packages/labtwopckg/src/move_square.py
+++ packages/labtwopckg/src/move_square.py
@@ -3,6 +3,7 @@
 
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
+from duckietown_msgs.msg import FSMState
 
 
 class Talker:
@@ -13,18 +14,44 @@ class Talker:
         self.pub.publish(msg)
 
 
+class Listener:
+    def __init__(self):
+        rospy.Subscriber('fsm_node/mode', FSMState, self.callback)
+        self.t = Talker()
+    
+    def callback(self, msg):
+        # check message for a mode switch
+        if not msg.state is "LANE_FOLLOWING":
+            return
+        # move in a square
+        rate = rospy.Rate(1) # 1hz
+        move_cmd = Twist2DStamped()
+        move_cmd.omega = 0
+        while (true):
+            # forward 1 meter
+            move_cmd.v = 1
+            self.t.talk(move_cmd)
+            rate.sleep()
+            # wait 5 seconds
+            move_cmd.v = 0
+            self.t.talk(move_cmd)
+            for i in range(5):
+                rate.sleep()
+            # turn 90 degrees
+            move_cmd.omega = 1
+            self.t.talk(move_cmd)
+            rate.sleep()
+            # wait 5 seconds
+            move_cmd.omega = 0
+            self.t.talk(move_cmd)
+            for i in range(5):
+                rate.sleep()
+
+
 if __name__ == '__main__':
     try:
         rospy.init_node('move_square', anonymous=True)
         t = Talker()
-        rate = rospy.Rate(1) # 1hz
-        move_cmd = Twist2DStamped()
-        move_cmd.v = 1
-        move_cmd.omega = 0
-        t.talk(move_cmd)
-        rate.sleep()
-        move_cmd.v = 0
-        t.talk(move_cmd)
     except rospy.ROSInterruptException:
         pass
 
