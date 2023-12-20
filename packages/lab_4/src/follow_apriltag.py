9313c22d86b37205667f52e99e9dde4615a2f3cb
Nolan Allen <nolan_allen@student.uml.edu>
2023-12-20 17:20:55 -0500

    rotational pid

--- packages/lab_4/src/follow_apriltag.py
+++ packages/lab_4/src/follow_apriltag.py
@@ -4,6 +4,38 @@
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
 from duckietown_msgs.msg import FSMState
+from duckietown_msgs.msg import AprilTagDetectionArray
+
+
+class PIDController:
+    def __init__(self, kp, ki, kd):
+        self.kp = kp # proportional
+        self.ki = ki # integral
+        self.kd = kd # derivative
+        self.sum = 0
+        self.initialize = True
+        self.timestamp = 0
+        self.pre_error = 0
+    
+    def get_accel(self, error):
+        if (self.initialize):
+            self.timestamp = rospy.get_time()
+            self.pre_error = error
+            self.initialize = False
+        
+        delta_error = error - self.pre_error
+        delta_time = rospy.get_time() - self.timestamp
+        self.sum += delta_error * delta_time
+        p = self.kp * error
+        i = self.ki * self.sum
+        d = 0
+        if (delta_time != 0):
+            d = self.kd * (delta_error / delta_time)
+        
+        self.timestamp = rospy.get_time()
+        self.pre_error = error
+        
+        return p + i + d
 
 
 class Talker:
@@ -16,56 +48,47 @@ class Talker:
 
 class Listener:
     def __init__(self):
-        rospy.Subscriber('fsm_node/mode', FSMState, self.callback)
+        #rospy.Subscriber('fsm_node/mode', FSMState, self.start_callback)
+        rospy.Subscriber('apriltag_detector_node/detections', AprilTagDetectionArray, self.callback)
         self.t = Talker()
         self.running = False
+        self.tag_detected = False
+        self.linear_controller = PIDController(1,0,0)
+        self.rotational_controller = PIDController(10,0,0)
     
-    def callback(self, msg):
+    def start_callback(self, msg):
         # check message for a mode switch
         if not msg.state == 'LANE_FOLLOWING':
             return
         if self.running:
             return
         self.running = True
-        # move in a square
-        rate = rospy.Rate(1) # 1hz
+
+    
+    def callback(self, msg):
+        # check for an apriltag
+        if msg.detections == []:
+            if self.tag_detected:
+                rospy.logwarn('no tag detected')
+                self.tag_detected = False
+            return
+        if not self.tag_detected:
+            rospy.logwarn('tag detected')
+            self.tag_detected = True
+        # calculate wheel velocities with the PID controllers
         move_cmd = Twist2DStamped()
-        move_cmd.omega = 0
-        #while True:
-        for i in range(4):
-            rospy.logwarn(i)
-            # forward 1 meter
-            move_cmd.v = rospy.get_param('velocity')
-            move_cmd.omega = rospy.get_param('lean')
-            self.t.talk(move_cmd)
-            rate.sleep()
-            rate.sleep()
-            rate.sleep()
-            # wait 5 seconds
-            move_cmd.v = 0
-            move_cmd.omega = 0
-            self.t.talk(move_cmd)
-            for j in range(5):
-                rate.sleep()
-            # turn 90 degrees
-            move_cmd.omega = rospy.get_param('omega')
-            self.t.talk(move_cmd)
-            rate.sleep()
-            # wait 5 seconds
-            move_cmd.omega = 0
-            self.t.talk(move_cmd)
-            for j in range(5):
-                rate.sleep()
+        move_cmd.v = 0
+        move_cmd.omega = -self.rotational_controller.get_accel(msg.detections[0].transform.translation.x)
+        s = '\nInput: ' + str(msg.detections[0].transform.translation.x) + '\nCommand: ' + str(move_cmd.omega)
+        rospy.logwarn(s)
+        # publish the calculated velocities
+        self.t.talk(move_cmd)
 
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('move_square', anonymous=True)
-        rospy.set_param('velocity', 0.35)
-        rospy.set_param('omega', 0.7)
-        rospy.set_param('lean', 0.02) # slight turn on forward movement
+        rospy.init_node('follow_apriltag', anonymous=True)
         l = Listener()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
-
